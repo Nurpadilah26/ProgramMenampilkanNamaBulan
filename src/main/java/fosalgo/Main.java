373d5ba6da3dda8c426a975cf24347250fc58ac6
Nurpadilah (D0220340) <72734052+Nurpadilah26@users.noreply.github.com>
2021-03-03 11:54:58 +0000

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -1,35 +1,94 @@
-package fosalgo;
+import  java.util.Scanner ;
+ kelas  umum Utama {
+ private  static  final  Scanner input =  new  Scanner ( System . in);
+ public  static  void  main ( String [] args ) {
+ Scanner sc =  Scanner baru  ( System . In );
 
-import java.util.Scanner;
+ // TULISKAN KODEMU DIBAWAH INI
+ Sistem . keluar . println ( " Masukkan Bulan " );
+ int bulan;
 
-/**
- *
- * @author Sugiarto Cokrowibowo
- */
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        //TULISKAN KODEMU DIBAWAH INI
-        String namabulan[] = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
-
-        System.out.println("========");
-        System.out.println("Output: ");
-        if ((n >= 1) && (n <= 12)) {
-            // index array = 0
-            n--;
-            System.out.println(namabulan[n % 12]);
-            System.out.println(namabulan[(n + 1) %12]);
-            System.out.println(namabulan[(n + 2) %12]);
-            System.out.println(namabulan[(n + 3) %12]);
-        } else {
-            System.out.println("Input tidak valid");
-        }
-        
-        
-        
-    }
-
-}
+bulan = masukan . nextInt ();
+ if (bulan ==  1 )
+ {
+ Sistem . keluar . println ( " Januari " );
+ Sistem . keluar . println ( " Februari " );
+ Sistem . keluar . println ( " Maret " );
+ Sistem . keluar . println ( " April " );
+ }
+ if (bulan ==  2 )
+ {
+ Sistem . keluar . println ( " Februari " );
+ Sistem . keluar . println ( " Maret " );
+ Sistem . keluar . println ( " April " );
+ Sistem . keluar . println ( " Mei " );
+ }
+ if (bulan ==  3 )
+ {
+ Sistem . keluar . println ( " Maret " );
+ Sistem . keluar . println ( " April " );
+ Sistem . keluar . println ( " Mei " );
+ Sistem . keluar . println ( " Juni " );
+ }
+ if (bulan ==  4 )
+ {
+ Sistem . keluar . println ( " April " );
+ Sistem . keluar . println ( " Mei " );
+ Sistem . keluar . println ( " Juni " );
+ Sistem . keluar . println ( " Juli " );
+ }
+ if (bulan ==  5 )
+ {
+ Sistem . keluar . println ( " Mei " );
+ Sistem . keluar . println ( " Juni " );
+ Sistem . keluar . println ( " Juli " );
+ Sistem . keluar . println ( " Agustus " );
+ }
+ if (bulan ==  6 )
+ {
+ Sistem . keluar . println ( " Juni " );
+ Sistem . keluar . println ( " Juli " );
+ Sistem . keluar . println ( " Agustus " );
+ Sistem . keluar . println ( " September " );
+ }
+ if (bulan ==  7 )
+ {
+ Sistem . keluar . println ( " Juli " );
+ Sistem . keluar . println ( " Agustus " );
+ Sistem . keluar . println ( " September " );
+ Sistem . keluar . println ( " Oktober " );
+ }
+ if (bulan ==  8 )
+ {
+ Sistem . keluar . println ( " Agustus " );
+ Sistem . keluar . println ( " September " );
+ Sistem . keluar . println ( " Oktober " );
+ Sistem . keluar . println ( " November " );
+ }
+ if (bulan ==  9 )
+ {
+ Sistem . keluar . println ( " September " );
+ Sistem . keluar . println ( " Oktober " );
+ Sistem . keluar . println ( " November " );
+ Sistem . keluar . println ( " Desember " );
+ }
+ if (bulan ==  10 )
+ {
+ Sistem . keluar . println ( " Oktober " );
+ Sistem . keluar . println ( " November " );
+ Sistem . keluar . println ( " Desember " );
+ Sistem . keluar . println ( " Januari " );
+ }
+ if (bulan ==  11 )
+ {
+ Sistem . keluar . println ( " November " );
+ Sistem . keluar . println ( " Desember " );
+ Sistem . keluar . println ( " Januari " );
+ Sistem . keluar . println ( " Februari " );
+ }
+ if (bulan ==  12 )
+ {
+ Sistem . keluar . println ( " Desember " );
+ Sistem . keluar . println ( " Januari " );
+ Sistem . keluar . println ( " Februari " );
+ Sistem .
